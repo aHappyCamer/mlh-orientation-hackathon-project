dc388747af3b9a01d7e44f5ca4490e63f00f7ecd
Joey <joeydcam@gmail.com>
2021-07-04 01:57:22 +0000

    feat: add database connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,26 @@
 import os
+from flask import Flask, render_template
+from . import db
+
+app = Flask(__name__)
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
+
+import os
+from flask import Flask, request, render_template
+
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+
+import os
 from flask import Flask, render_template, send_from_directory
 from dotenv import load_dotenv
 
 load_dotenv()
-app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return render_template('index.html',  url=os.getenv("URL"))
+
+@app.route('/health')
+def check():
+    return 'Works'
