355afc0a70349b00f3d3ee8e944b5f7304fffc98
Joey <joeydcam@gmail.com>
2021-07-04 21:26:33 +0000

    refactor: remove redundant code

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,18 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, request, render_template, send_from_directory
+from dotenv import load_dotenv
 from . import db
+#... for /register
+from werkzeug.security import generate_password_hash
+from app.db import get_db
+#... for /login
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.db import get_db
 
+load_dotenv()
 app = Flask(__name__)
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 db.init_app(app)
-
-import os
-from flask import Flask, request, render_template
-
-app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
-
-import os
-from flask import Flask, render_template, send_from_directory
-from dotenv import load_dotenv
-
-load_dotenv()
 
 @app.route('/')
 def index():
@@ -24,3 +21,59 @@ def index():
 @app.route('/health')
 def check():
     return 'Works'
+
+# /register
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a register page
+    return "Register Page not yet implemented", 501
+
+#... /login
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return "Login Page not yet implemented", 501
