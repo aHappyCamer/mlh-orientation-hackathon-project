50edc8011185ff693559bb9196ac55f75c093f89
Lint Action <lint-action@samuelmeuli.com>
2021-07-08 23:53:42 +0000

    Fix code style issues with Black

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}'.format(
-	user=os.getenv('POSTGRES_USER'),
-	passwd=os.getenv('POSTGRES_PASSWORD'),
-	host=os.getenv('POSTGRES_HOST'),
-	port=5432,
-	table=os.getenv('POSTGRES_DB'))
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{table}".format(
+    user=os.getenv("POSTGRES_USER"),
+    passwd=os.getenv("POSTGRES_PASSWORD"),
+    host=os.getenv("POSTGRES_HOST"),
+    port=5432,
+    table=os.getenv("POSTGRES_DB"),
+)
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class UserModel(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     username = db.Column(db.String(), primary_key=True)
     password = db.Column(db.String())
@@ -30,25 +34,28 @@ class UserModel(db.Model):
     def __repr__(self):
         return f"<User {self.username}>"
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html',  url=os.getenv("URL"))
+    return render_template("index.html", url=os.getenv("URL"))
 
-@app.route('/health')
+
+@app.route("/health")
 def check():
-    return 'Works'
+    return "Works"
 
-@app.route('/register', methods=('GET', 'POST'))
+
+@app.route("/register", methods=("GET", "POST"))
 def register():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
 
         if not username:
-            error = 'Username is required.'
+            error = "Username is required."
         elif not password:
-            error = 'Password is required.'
+            error = "Password is required."
         elif UserModel.query.filter_by(username=username).first() is not None:
             error = f"User {username} is already registered."
 
@@ -64,18 +71,18 @@ def register():
     return "Register Page not yet implemented", 501
 
 
-@app.route('/login', methods=('GET', 'POST'))
+@app.route("/login", methods=("GET", "POST"))
 def login():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
         error = None
         user = UserModel.query.filter_by(username=username).first()
 
         if user is None:
-            error = 'Incorrect username.'
+            error = "Incorrect username."
         elif not check_password_hash(user.password, password):
-            error = 'Incorrect password.'
+            error = "Incorrect password."
 
         if error is None:
             return "Login Successful", 200
